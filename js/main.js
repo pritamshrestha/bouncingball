12e0d91cd22dc85952cc8e99e887e1c8382c758c
pritam shrestha <safal_pritam@hotmail.com>
2017-02-07 10:07:48 -0800

    first commit

--- js/main.js
+++ js/main.js
@@ -1,82 +1,177 @@
-// this is for the js
-window.onload=function () {
-    
-    // get the reference from the canvas 
-    var canvas=document.getElementById("mycanvas");
-    var ctx=canvas.getContext("2d");
-    // this is for the multiple circle
-    var circles=[{x:100,y:200,r:10,color:"blue",vx:1,vy:-1},
-                 {x:100,y:300,r:12,color:"red",vx:2,vy:-2},
-                 {x:200,y:100,r:18,color:"green",vx:3,vy:-3},
-                 {x:300,y:150,r:10,color:"pink",vx:1.6,vy:-1},
-                 {x:340,y:250,r:15,color:"orange",vx:2,vy:2},
-                 {x:200,y:120,r:25,color:"white",vx:1.9,vy:-3},
-                 {x:150,y:100,r:9,color:"golden",vx:2.6,vy:-1},
-                 {x:201,y:130,r:13,color:"yellow",vx:2,vy:1},
-                 {x:100,y:180,r:22,color:"purple",vx:1,vy:-3},
-                 {x:70,y:190,r:29,color:"red",vx:2,vy:-3},
-                 {x:20,y:19,r:33,color:"orange",vx:4,vy:-1},
-                 {x:380,y:250,r:17,color:"violet",vx:3,vy:1},
-                 {x:300,y:90,r:25,color:"blue",vx:2,vy:2.5},
-                 ];
-    // create the some variables for the position x and the y
-    var x=canvas.width/2;
-    var y=canvas.height/2;
-    // creating the two variables for the speed 
-   // var vx=2;
-   // var vy=-2;
+// this is for reference to render the content of the javascript
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 15;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 6;
+var dy = -6;
+var paddleHeight = 10;
+var paddleWidth = 90;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 10;
+var brickColumnCount = 9;
+var brickWidth = 75;
+var brickHeight = 30;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var score = 0;
+var lives = 3;
+// creating the brick object
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
 
-    // here we call the function
-    Draw();
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
-// creating the function draw
-function Draw(){
-   
-    // this is for refreshing the canvas
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    // for multiple circle
-    for(i=0;i<circles.length; i++){
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+function collisionDetection() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATS!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+// this is for the ball
+function drawBall() {
     ctx.beginPath();
-    // creating the red circle
-    ctx.arc(circles[i].x,circles[i].y,circles[i].r,0,Math.PI*2);//for multiple circle
-
-    ctx.fillStyle=circles[i].color;
-    ctx.font = "30px Arial";//this is for the font
-    ctx.fillText("Bouncing Ball",10,50);
-    ctx.fill();//fill it
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
     ctx.closePath();
+}
+// this is for the paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+// this is for the bricks
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                // this is fo generating the random color
+                //ctx.fillStyle = 'hsl('+180*Math.random() +',100%,50%)';
+               
+                ctx.fillStyle="red";
+                ctx.strokeStyle="blue";
+                ctx.stroke();
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+// this is for the score
+function drawScore() {
+    ctx.font = "25px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: "+score*1000, 10, 20);
+}
+// this is for the lives
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
 
-
+function draw() {
+// to clear the privious position of the ball and the paddle
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawLives();
+    collisionDetection();
     
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            lives--;
+            if(!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width/2;
+                y = canvas.height-30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width-paddleWidth)/2;
+            }
+        }
+    }
     
-    // this is for the wall colliding conditions
-
-if(circles[i].x+circles[i].vx>800){
-    circles[i].vx=-circles[i].vx;
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
     
-}
-if(circles[i].x+circles[i].vx<0)
-{
-circles[i].vx=-circles[i].vx;
+    x += dx;
+    y += dy;
+    requestAnimationFrame(draw);//for the animation
 }
 
-if(circles[i].y+circles[i].vy>500)
-{
-    circles[i].vy=-circles[i].vy;
-
-}
-if(circles[i].y+circles[i].vy<0)
-{
-circles[i].vy=-circles[i].vy;
-}
-// here we update the values
-circles[i].x+=circles[i].vx;
-circles[i].y+=circles[i].vy;
-// to make this like animation we need ot use this method
-// where we pass the function draw as an argument.
-}//for multiple circle
-}
-setInterval(Draw,10);//this is for the animation speed
-
-
-}
+draw();//calling the function draw.
